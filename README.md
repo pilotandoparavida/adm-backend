38f75ace340c3a66fb29c33eff07d35994da0e0a
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-06 08:56:26 -0300

    Primira estrutura

---
+++ README.md
@@ -0,0 +1,13 @@
+## Pilotando Para Vida
+
+```
+1- mkdir backend
+2- yarn init -y
+3- yarn add express
+    acelerar a construcao da aplicação
+4- yarn add nodemon -D
+    para auxiliar na deteccao de alteracoes no arquivo
+5- yarn add mongoose 
+    facilitar trabalhar com dados no mongodb
+6- yarn add cors
+```
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+let path_controller = 'controller_ppv'
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    path_controller = 'controller_ppv_dev';
+}
+const {TurmaController, AlunoController, MotoController, AdministradorController} = require(path_controller);
+const routes = express.Router();
+
+routes.get('/', (req, res) =>{
+    res.status(404).send("Implementação de serviços para versão administrativa do aplicativo Pilotando Para Vida!");
+});
+
+routes.get('/turma/:turma_id', TurmaController.show);
+routes.post('/turma/cadastro', TurmaController.store); 
+
+routes.get('/turma', TurmaController.list);
+
+routes.get('/aluno/:aluno_id', AlunoController.show);
+routes.get('/aluno', AlunoController.index);
+
+routes.get('/moto', MotoController.index);
+routes.get('/moto/:aluno_id', MotoController.show);
+
+routes.get('/login', AdministradorController.show);
+
+routes.post('/rsqEqBAQQfdVZTAAeLMr', AdministradorController.store);
+
+module.exports = routes;
